d9ed12756a5f11395fc69db92d5097ef074d964d
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-02-14 10:15:48 +0530

    Added:Exception if Inform user entered invalid input

--- MoodAnalyserProblem/src/com/moodanalyser/MoodAnalyser.java
+++ MoodAnalyserProblem/src/com/moodanalyser/MoodAnalyser.java
@@ -2,15 +2,17 @@ package com.moodanalyser;
 
 public class MoodAnalyser {
 	
-	public String AnalyseMood(String message) {
-		try{
+	public String AnalyseMood(String message) throws MoodAnalyserException {
+		try {
+			if(message.length() == 0)
+				throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.ENTERED_EMPTY,"Please Enter Mood Properly");
 			if(message.contains("Sad"))
 				return "SAD";
 			else
 				return "HAPPY";
-		} catch(Exception e) {
-			System.out.println("In Catch Block");
-			return "HAPPY";
-		}
+		} catch(NullPointerException e) {
+			System.out.println(" In Catch Block Null pointer Exception");
+			throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.ENTERED_NULL,"Entered Invalid Mood");
+		} 
 	}
 }
