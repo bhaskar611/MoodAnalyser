1587f052fc81f12edee10795a010363b259d53fb
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-02-14 09:29:14 +0530

    Added:Handle Exception if user provide invalid input

--- MoodAnalyserProblem/src/com/moodanalyser/MoodAnalyserTest.java
+++ MoodAnalyserProblem/src/com/moodanalyser/MoodAnalyserTest.java
@@ -11,14 +11,23 @@ public class MoodAnalyserTest {
 	@Test 
 	public void Test1() {
 		MoodAnalyser moodAnalyser= new MoodAnalyser();
-		String mood = moodAnalyser.AnalyseMood("This is a Sad Message");
+		
+		String mood = moodAnalyser.AnalyseMood("I am in a Sad Mood");
 		Assert.assertEquals("SAD", mood);
 	}
 
 	@Test 
 	public void Test2() {
 		MoodAnalyser moodAnalyser= new MoodAnalyser();
-		String mood = moodAnalyser.AnalyseMood("This is a Happy Message");
+		
+		String mood = moodAnalyser.AnalyseMood("I am in a Happy Mood");
+		Assert.assertEquals("HAPPY", mood);
+	}
+	@Test 
+	// Handle exception if user provide invalid input
+	public void Test3() {
+		MoodAnalyser moodAnalyser= new MoodAnalyser();
+		String mood = moodAnalyser.AnalyseMood(null);
 		Assert.assertEquals("HAPPY", mood);
 	}
 
